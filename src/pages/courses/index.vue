5874ea1c147d07df9ad779f8ed4ca8ab39f34065
siraphop-oLe <siraphop.rmutt@gmail.com>
2025-01-27 19:22:24 +0700

    add page

---
+++ src/pages/courses/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Courses Page</h1>
+    <p>This is the Courses page.</p>
+  </div>
+</template>
---
+++ src/pages/workshops/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Workshops Page</h1>
+    <p>This is the Workshops page.</p>
+  </div>
+</template>
